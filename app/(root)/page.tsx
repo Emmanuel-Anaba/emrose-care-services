ee1b08ed02082ab9b6660d1c595541d12c5d4ece
Emmanuel-Anaba <emmaanaba29@gmail.com>
2024-11-28 08:31:50 +0100

    Modified homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <main>Nothing to see here </main>;
+"use client";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+
+export default function HomePage() {
+  const [text] = useTypewriter({
+    words: ["health training", "coaching", "wellness services"],
+    loop: Infinity,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <main className="grid">
+      <header>
+        <div>
+          <h1>Welcome to EmRose Care Services!</h1>
+          <p>
+            We offer top-tier
+            <span>
+              {` ${text}`}
+              <Cursor />
+            </span>
+          </p>
+        </div>
+      </header>
+    </main>
+  );
 }
